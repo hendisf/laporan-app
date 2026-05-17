0975225f85032dbfae5660996ed0a50efcbd8074
sintaratih <sintaratih53@gmail.com>
2026-05-17 21:49:20 +0700

    Create loading.tsx

---
+++ app/laporan/loading.tsx
@@ -0,0 +1,20 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col gap-4 p-6">
+      <div className="h-8 w-40 bg-gray-300 rounded animate-pulse"></div>
+
+      {[1, 2, 3].map((item) => (
+        <div
+          key={item}
+          className="p-4 border rounded-lg space-y-2"
+        >
+          <div className="h-5 w-32 bg-gray-300 rounded animate-pulse"></div>
+
+          <div className="h-4 w-full bg-gray-200 rounded animate-pulse"></div>
+
+          <div className="h-4 w-3/4 bg-gray-200 rounded animate-pulse"></div>
+        </div>
+      ))}
+    </div>
+  )
+}
