525f7f4db576012af2a28e0ed00ae873fe87ffd3
sintaratih <sintaratih53@gmail.com>
2026-05-17 21:06:43 +0700

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,38 +1,21 @@
 export default function Loading() {
   return (
-    <div className="p-6 space-y-4">
-      
-      {/* HEADER */}
-      <div className="h-8 w-60 bg-gray-300 rounded animate-pulse"></div>
+    <div className="flex flex-col gap-4 p-6">
+      <div className="h-8 w-40 bg-gray-300 rounded animate-pulse"></div>
 
-      {/* CARD */}
-      <div className="bg-white shadow-md rounded-xl p-5 space-y-4">
-        
-        <div className="h-6 w-40 bg-gray-300 rounded animate-pulse"></div>
+      {[1, 2, 3].map((item) => (
+        <div
+          key={item}
+          className="p-4 border rounded-lg space-y-2"
+        >
+          <div className="h-5 w-32 bg-gray-300 rounded animate-pulse"></div>
 
-        <div className="space-y-2">
           <div className="h-4 w-full bg-gray-200 rounded animate-pulse"></div>
-          <div className="h-4 w-5/6 bg-gray-200 rounded animate-pulse"></div>
-          <div className="h-4 w-4/6 bg-gray-200 rounded animate-pulse"></div>
-        </div>
 
-        {/* LIST SKELETON */}
-        <div className="space-y-3 pt-4">
-          {[1, 2, 3].map((item) => (
-            <div
-              key={item}
-              className="flex items-center justify-between border p-3 rounded-lg"
-            >
-              <div className="space-y-2 w-full">
-                <div className="h-4 w-1/2 bg-gray-300 rounded animate-pulse"></div>
-                <div className="h-3 w-1/3 bg-gray-200 rounded animate-pulse"></div>
-              </div>
-
-              <div className="h-8 w-20 bg-gray-300 rounded animate-pulse"></div>
-            </div>
-          ))}
+          <div className="h-4 w-3/4 bg-gray-200 rounded animate-pulse"></div>
         </div>
-      </div>
+      ))}
     </div>
-  );
+  )
+});
         }
